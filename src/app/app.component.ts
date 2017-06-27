f231f68f53db24276e71ab0d2bda28dcd43ec721
Sokos6 <will.sokolowski@gmail.com>
2017-06-27 17:23:58 -0400

    initial api connection completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Book } from './book';
+import { OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  
+  booksList: Book[] = [];
+  book: Book;
+  baseUrl: string = 'http://localhost:4567';
+
+  constructor(private http: Http) {}
+
+  ngOnInit() {
+  	this.getBooksData();
+  }
+
+  getBooksData() {
+  	const url = `${this.baseUrl}/books`;
+  	this.http.get(url).subscribe(res => this.booksList = res.json() as Book[]);
+  	console.log(this.booksList);
+  }
+
+  getBookInfo(id: number) {
+  	const url = `${this.baseUrl}/books/${id}`;
+  	this.http.get(url).subscribe(res => this.book = res.json() as Book);
+  }
 }
